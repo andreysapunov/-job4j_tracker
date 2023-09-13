7d95a76fa96062aa11d46ed402ad44417b6b465e
SapunovAU <squ1k@mail.ru>
2023-09-13 15:57:09 +0400

    [#504981

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String VALUE = "";
+    private int size = 10;
+    public String surname;
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    void echo() {
+    }
+
+    String name;
+
+    Broken() {
+    }
+}
